ecadf91d322b08a19f4a23fc2b9c822386050107
paulapura <s25180@pjwstk.edu.pl>
2024-03-24 21:00:50 +0100

    [T-BE-0] Init Turi backend project.

---
+++ turi-backend/src/main/java/com/turi/TuriBackendApplication.java
@@ -0,0 +1,13 @@
+package com.turi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuriBackendApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TuriBackendApplication.class, args);
+    }
+}
---
+++ turi-backend/src/test/java/com/turi/TuriBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.turi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuriBackendApplicationTests
+{
+    @Test
+    void contextLoads()
+    {
+
+    }
+}
