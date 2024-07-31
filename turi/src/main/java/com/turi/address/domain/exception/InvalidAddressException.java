83974a2d0976809c6e382fcd854d9195591d8004
„mateuszkowalczuk” <mateuszk100801@gmail.com>
2024-07-31 15:27:41 +0200

    [T-3] Dodanie implementacji wyjątku dla encji Adres.

---
+++ turi/src/main/java/com/turi/address/domain/exception/InvalidAddressException.java
@@ -0,0 +1,17 @@
+package com.turi.address.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.Serial;
+
+public final class InvalidAddressException extends ResponseStatusException
+{
+    @Serial
+    private static final long serialVersionUID = -219099189628208745L;
+
+    public InvalidAddressException()
+    {
+        super(HttpStatus.BAD_REQUEST, "The specified address is invalid. Parameters country, city, zipCode street and buildingNumber are required!");
+    }
+}
