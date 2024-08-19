c49f1c08bf6915d2224ad7de7ac239fc952ea300
„mateuszkowalczuk” <mateuszk100801@gmail.com>
2024-08-19 11:42:26 +0200

    [T-3] Dopracowanie wyjątku dla niepoprawnego adresu.

--- turi/src/main/java/com/turi/address/domain/exception/InvalidAddressException.java
+++ turi/src/main/java/com/turi/address/domain/exception/InvalidAddressException.java
@@ -1,17 +1,14 @@
 package com.turi.address.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
 import java.io.Serial;
 
-public final class InvalidAddressException extends ResponseStatusException
+public final class InvalidAddressException extends RuntimeException
 {
     @Serial
     private static final long serialVersionUID = -219099189628208745L;
 
     public InvalidAddressException()
     {
-        super(HttpStatus.BAD_REQUEST, "The specified address is invalid. Parameters country, city, zipCode, street and buildingNumber are required!");
+        super("The specified address is invalid. Parameters country, city, zipCode, street and buildingNumber are required!");
     }
 }
