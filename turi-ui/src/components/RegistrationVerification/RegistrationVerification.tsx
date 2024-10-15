cb4a80316a5f2d01b130e96f99c0f59d975edeee
„mateuszkowalczuk” <mateuszk100801@gmail.com>
2024-10-15 10:54:29 +0200

    [T-4] Uzupełnienie komponentów.

---
+++ turi-ui/src/components/RegistrationVerification/RegistrationVerification.tsx
@@ -0,0 +1,18 @@
+// import AuthPanel from '../AuthPanel'
+import styles from './RegistrationVerification.module.css'
+
+const RegistrationVerification = () => {
+    return (
+        <div className={styles.verification}>
+            {/*<AuthPanel*/}
+            {/*    header={}*/}
+            {/*    option={}*/}
+            {/*    input={}*/}
+            {/*    button={}*/}
+            {/*    back={}*/}
+            {/*/>*/}
+        </div>
+    )
+}
+
+export default RegistrationVerification;
