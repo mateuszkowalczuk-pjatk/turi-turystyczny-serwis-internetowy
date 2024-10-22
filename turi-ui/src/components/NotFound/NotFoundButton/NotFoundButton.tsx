dfe404c32397334af9c72669aa5550a567749259
paulapura <s25180@pjwstk.edu.pl>
2024-10-22 17:38:04 +0200

    [T-4] Skompletowanie strony Not Found.

---
+++ turi-ui/src/components/NotFound/NotFoundButton/NotFoundButton.tsx
@@ -0,0 +1,24 @@
+import { useTranslation } from 'react-i18next'
+import { GreenButton } from '../../Controls/Button'
+import styles from './NotFoundButton.module.css'
+import {useNavigate} from 'react-router-dom'
+
+const NotFoundButton = () => {
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+
+    const navigateToHome = () => {
+        navigate('/')
+    }
+
+    return (
+        <div className={styles.button}>
+            <GreenButton
+                text={t('not-found.button')}
+                onClick={navigateToHome}
+            />
+        </div>
+    )
+}
+
+export default NotFoundButton
--- turi-ui/src/pages/NotFound/NotFoundPage.tsx
+++ turi-ui/src/pages/NotFound/NotFoundPage.tsx
@@ -1,5 +1,5 @@
 import AuthHeader from '../../components/Header/AuthHeader'
-import NotFound from '../../components/NotFound/NotFound.tsx'
+import NotFoundContent from '../../components/NotFound/NotFoundContent'
 import ProfileFooter from '../../components/Footer/ProfileFooter'
 import styles from '../Page.module.css'
 
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
     return (
         <div className={styles.page}>
             <AuthHeader />
-            <NotFound />
+            <NotFoundContent />
             <ProfileFooter />
         </div>
     )
