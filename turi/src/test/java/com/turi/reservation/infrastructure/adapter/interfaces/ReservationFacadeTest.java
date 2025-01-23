95e7fd9feff1a98914edde5d10dfae546edddd0a
„mateuszkowalczuk” <mateuszk100801@gmail.com>
2025-01-23 22:07:46 +0100

    [T-10] Skompletowanie backendu.

--- turi/src/test/java/com/turi/reservation/infrastructure/adapter/interfaces/ReservationFacadeTest.java
+++
@@ -1,9 +0,0 @@
-package com.turi.reservation.infrastructure.adapter.interfaces;
-
-import com.turi.testhelper.annotation.RestControllerTest;
-
-@RestControllerTest
-class ReservationFacadeTest
-{
-
-}
