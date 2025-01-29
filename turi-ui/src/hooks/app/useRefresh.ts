1c6834d0d2362dfeb4df2080123e9152604f2659
„mateuszkowalczuk” <mateuszk100801@gmail.com>
2025-01-29 18:03:37 +0100

    [T-2] Poprawienie automatyczengo odświeżania tokena w UI.

--- turi-ui/src/hooks/app/useRefresh.ts
+++ turi-ui/src/hooks/app/useRefresh.ts
@@ -6,7 +6,12 @@ export const useRefresh = () => {
     useEffect(() => {
         const interval = setInterval(async () => {
             const response = await authService.refresh()
-            if (response.status !== 200) logout()
+            if (response.status !== 200) {
+                console.error('Failed to refresh token')
+                logout()
+            } else {
+                console.error('Token refreshed')
+            }
         }, 840000)
         return () => clearInterval(interval)
     }, [])
